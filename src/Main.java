583f0b66582b8dfa7c0379e9d2c6f9f20076703c
Vasilliy <Vasily.vvk@gmail.com>
2022-10-20 20:03:05 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int x = 3;
+        int y = 2;
+        String a = "bear";
+        String b = "yellow";
+        System.out.println("Hello world"+ " " + a);
+        System.out.println(x+y);
+
+
+
+    }
+}
+
